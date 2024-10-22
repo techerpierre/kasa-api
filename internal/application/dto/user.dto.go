e37e60b88efe64d591d70f1632b8d1ed734ab50e
techernoelpierre <techernoelpierre@gmail.com>
2024-10-22 09:55:48 +0400

    feat: create user dto

---
+++ internal/application/dto/user.dto.go
@@ -0,0 +1,43 @@
+package dto
+
+import "github.com/techerpierre/kasa-api/internal/domain/entities"
+
+type UserDTO struct {
+	ID               string  `json:"id"`
+	Email            string  `json:"email"`
+	Firstname        string  `json:"firstname"`
+	Lastname         string  `json:"lastname"`
+	Picture          *string `json:"picture"`
+	Cover            *string `json:"cover"`
+	AuthorizationsID string  `json:"authorizationsId"`
+	Authorizations   any     `json:"authorizations"`
+	Accomodations    []any   `json:"accomodations"`
+	Bookings         []any   `json:"bookings"`
+	Ratings          []any   `json:"ratings"`
+	Comments         []any   `json:"comments"`
+}
+
+type UserInputDTO struct {
+	Email            string  `json:"email"`
+	Password         string  `json:"password"`
+	Firstname        string  `json:"firstname"`
+	Lastname         string  `json:"lastname"`
+	Picture          *string `json:"picture"`
+	Cover            *string `json:"cover"`
+	AuthorizationsID string  `json:"authorizationsId"`
+}
+
+func PipeUserInDTO(source *entities.User, target *UserDTO) {
+	target.ID = source.ID
+	target.Email = source.Email
+	target.Firstname = source.Firstname
+	target.Lastname = source.Lastname
+	target.Picture = source.Picture
+	target.Cover = source.Cover
+	target.AuthorizationsID = source.AuthorizationsID
+	target.Authorizations = source.Authorizations
+	target.Accomodations = nil
+	target.Bookings = nil
+	target.Ratings = nil
+	target.Comments = nil
+}
